89d19d7e5db603cd7a8e357c34946b656599e7c6
Luke Inlow <lukeinlow@gmail.com>
2025-02-06 14:34:20 -0500

    Disabled linting in build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'export',
+	distDir: 'dist',
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
