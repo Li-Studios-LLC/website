cd507873f689c3b1768483f28a6c9096fed42bdf
Luke Inlow <lukeinlow@gmail.com>
2025-02-12 13:56:50 -0500

    Working on styling for mobile

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,19 +1,20 @@
 import './TopBar.css';
+import Link from 'next/link';
 
 export default function TopBar() {
 	return (
 		<nav className='TopBar'>
-			<a href='/'>
+			<Link href='/'>
 				<img src='logos/exports/WideInverted.png' alt='Logo' />
-			</a>
+			</Link>
 
-			<a href='/about'>
+			<Link href='/about' prefetch={true}>
 				<h1>About</h1>
-			</a>
+			</Link>
 
-			<a href='/contact'>
+			<Link href='/contact' prefetch={true}>
 				<h1>Contact</h1>
-			</a>
+			</Link>
 		</nav>
 	);
 }
