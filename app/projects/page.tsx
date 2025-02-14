9810ffc9d57b4a44afa7ccc9761761602b01f82e
Luke Inlow <lukeinlow@gmail.com>
2025-02-14 01:04:41 -0500

    Working on project component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,7 @@
 import './projects.css';
 import type { Metadata } from 'next';
 import Project from '@/components/Project';
+import logo from '@/public/logos/exports/WideInverted.png';
 
 export const metadata: Metadata = {
 	title: 'Li Studios - Projects',
@@ -10,7 +11,8 @@ export default function Projects() {
 	return (
 		<div className='PageSection Projects'>
 			<h1>Projects</h1>
-			<Project name='liTech' />
+			<Project name='liTech' description='The in house and open source game engine used for our projects.' alt='liTech' image={logo} />
+			<Project name='This Site' description='This website was written in house, using React and NextJS.' alt='This Site' image={logo} />
 		</div>
 	);
 }
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,11 +1,18 @@
+import Image, { StaticImageData } from 'next/image';
+
 export interface ProjectProps {
-	name: String;
+	name: string;
+	description: string;
+	alt: string;
+	image: string | StaticImageData;
 }
 
 export default function Project(props: ProjectProps) {
 	return (
 		<div>
 			<h2>{props.name}</h2>
+			<p>{props.description}</p>
+			<Image src={props.image} alt={props.alt} priority />
 		</div>
 	);
 }
